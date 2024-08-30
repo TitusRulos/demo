672560ca4a66a1238618ea22f66057b384b64690
TitusRulos <jujotiga@gmail.com>
2024-08-30 17:08:01 -0500

    Comienzo de login

--- src/main/java/com/example/demo/entities/rol/domain/entity/Rol.java
+++ src/main/java/com/example/demo/entities/rol/domain/entity/Rol.java
@@ -1,8 +1,8 @@
-package com.example.demo.entities.user.domain.entity;
+package com.example.demo.entities.rol.domain.entity;
 
 import java.util.Set;
 
-import com.example.demo.entities.rol.domain.entity.Rol;
+import com.example.demo.entities.user.domain.entity.User;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
@@ -16,18 +16,17 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="user")
-public class User {
+@Table(name="rol")
+public class Rol {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private String password;
 
     @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id"),
-    inverseJoinColumns = @JoinColumn(name = "rol_id"))
-    private Set<Rol> roles;
+    @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "rol_id"),
+    inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private Set<User> users;
 
     public Long getId() {
         return id;
@@ -45,19 +44,13 @@ public class User {
         this.name = name;
     }
 
-    public String getPassword() {
-        return password;
+    public Set<User> getUsers() {
+        return users;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setUsers(Set<User> users) {
+        this.users = users;
     }
 
-    public Set<Rol> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Rol> roles) {
-        this.roles = roles;
-    }  
+    
 }
